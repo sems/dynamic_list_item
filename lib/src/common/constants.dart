0063e067321b34e7e4d7cbd6513bc8b97998d6cb
Sem <me@sems.dev>
2022-09-29 14:49:05 +0200

    Fixed constants

--- lib/src/common/constants.dart
+++ lib/src/common/constants.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class Constants {
   double get defaultHorizontalPadding => 16.0;
 
-  // iOS global constants
-  Color get iosBackgroundColor => Color.fromRGBO(239, 239, 244, 1.0);
-  Color get iosListTileDownColor => Color.fromRGBO(224, 222, 230, 0.5);
-  Color get iosListTileArrowColor => Color.fromRGBO(211, 211, 211, 1.0);
+  Color get iosBackgroundColorLight => Color.fromRGBO(255, 255, 255, 1.0);
+  Color get iosListTileDownColorLight => Color.fromRGBO(245, 245, 247, 1.0);
+  Color get iosListTileArrowColor => Color.fromRGBO(197, 197, 199, 1.0);
 
-  // Android global constants
+  Color get iosBackgroundColorDark => Color.fromRGBO(29, 28, 30, 1.0);
+  Color get iosListTileDownColorDark => Color.fromRGBO(44, 44, 46, 1.0);
+
+  Color get iosBackgroundColor => SchedulerBinding.instance.window.platformBrightness == Brightness.dark
+      ? iosBackgroundColorDark
+      : iosBackgroundColorLight;
+
+  Color get iosListTileDownColor => SchedulerBinding.instance.window.platformBrightness == Brightness.dark
+      ? iosListTileDownColorDark
+      : iosListTileDownColorLight;
+
   TextStyle get androidPrimaryTextStyle => TextStyle(fontSize: 18);
 }
